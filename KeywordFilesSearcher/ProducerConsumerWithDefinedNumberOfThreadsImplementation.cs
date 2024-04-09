cba437adc0b724c9cdc27d065509b0111efade26
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-04-09 17:26:32 +0300

    With 12 threads added

---
+++ KeywordFilesSearcher/ProducerConsumerWithDefinedNumberOfThreadsImplementation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Concurrent;
+
+namespace KeywordFilesSearcher;
+
+internal class ProducerConsumerWithDefinedNumberOfThreadsImplementation
+{
+    private readonly BlockingCollection<string> _filesWithKeyword = new();
+
+    public void ProduceAsync(string keyword, IEnumerable<string> textFiles)
+    {
+        var threads = Partitioner.Create(textFiles).GetPartitions(11)
+            .Select(filePaths =>
+            {
+                var thread = new Thread(() => ReadAndSearchForKeyword(filePaths));
+                thread.Start();
+                return thread;
+            });
+
+        Parallel.ForEach(threads, thread => thread.Join());
+        StopSearch();
+
+
+        void ReadAndSearchForKeyword(IEnumerator<string> filePaths)
+        {
+            using (filePaths)
+            {
+                while (filePaths.MoveNext())
+                {
+                    try
+                    {
+                        using StreamReader sr = new StreamReader(filePaths.Current);
+                        string? line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line.Contains(keyword, StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                _filesWithKeyword.Add(filePaths.Current);
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    public void Consume()
+    {
+        foreach (string file in _filesWithKeyword.GetConsumingEnumerable())
+        {
+            Console.WriteLine($"Found keyword in: {file}");
+        }
+    }
+
+    public void StopSearch() => _filesWithKeyword.CompleteAdding();
+}
