a4292e0a80fb86cdad62c53a3033d6cc6dda1764
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-04-09 17:26:05 +0300

    Async partition implementation added

---
+++ KeywordFilesSearcher/ProducerConsumerWithAsyncPartitionImplementation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Concurrent;
+
+namespace KeywordFilesSearcher;
+
+internal class ProducerConsumerPartitionAsync
+{
+    private readonly BlockingCollection<string> _filesWithKeyword = new();
+
+    public async Task ProduceAsync(string keyword, IEnumerable<string> textFiles)
+    {
+        await Task.WhenAll(Partitioner.Create(textFiles).GetPartitions(100).AsParallel().Select(ReadAndSearchForKeyword));
+        StopSearch();
+
+        async Task ReadAndSearchForKeyword(IEnumerator<string> filePaths)
+        {
+            using (filePaths)
+            {
+                while (filePaths.MoveNext())
+                {
+                    await Task.Run(() =>
+                    {
+                        try
+                        {
+                            using StreamReader sr = new StreamReader(filePaths.Current);
+                            string? line;
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                if (line.Contains(keyword, StringComparison.InvariantCultureIgnoreCase))
+                                {
+                                    _filesWithKeyword.Add(filePaths.Current);
+                                    break;
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    public void Consume()
+    {
+        foreach (string file in _filesWithKeyword.GetConsumingEnumerable())
+        {
+            Console.WriteLine($"Found keyword in: {file}");
+        }
+    }
+
+    public void StopSearch() => _filesWithKeyword.CompleteAdding();
+
+}
